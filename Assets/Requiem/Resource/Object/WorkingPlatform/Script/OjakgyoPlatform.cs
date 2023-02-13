cbb226dfc1167b8626b24925efb26e00794dad2f
HideThat <101398304+HideThat@users.noreply.github.com>
2023-02-13 16:01:33 +0900

    2023-02-13

--- Assets/Requiem/Resource/Object/WorkingPlatform/Script/OjakgyoPlatform.cs
+++ Assets/Requiem/Resource/Object/WorkingPlatform/Script/OjakgyoPlatform.cs
@@ -10,6 +10,7 @@ public class OjakgyoPlatform : MonoBehaviour
     [SerializeField] float m_desY;
     [SerializeField] float m_moveTime;
     [SerializeField] float m_timeLaps;
+    [SerializeField] AudioSource m_audioSource;
 
     Vector2 m_initialPos;
     Vector2 m_destyPos;
@@ -18,6 +19,7 @@ public class OjakgyoPlatform : MonoBehaviour
 
     private void Awake()
     {
+        m_audioSource.gameObject.SetActive(false);
         m_initialPos = transform.position;
         m_destyPos = new Vector2(m_desX, m_desY);
         m_delayTime = 0f;
@@ -57,11 +59,13 @@ public class OjakgyoPlatform : MonoBehaviour
         if (m_delayTime <= m_timeLaps && m_isActive)
         {
             transform.DOMove(m_destyPos, m_moveTime);
+            m_audioSource.gameObject.SetActive(true);
             m_delayTime += Time.deltaTime;
         }
         else if (m_delayTime > m_timeLaps)
         {
             transform.DOMove(m_initialPos, m_moveTime);
+            m_audioSource.gameObject.SetActive(false);
             m_delayTime = 0f;
             m_isActive = false;
         }
--- Assets/Requiem/Resource/Other/Script/Trigger/DisapearTrigger.cs
+++ Assets/Requiem/Resource/Other/Script/Trigger/DisapearTrigger.cs
@@ -2,13 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.Tilemaps;
 using DG.Tweening;
 
 
 public class DisapearTrigger : MonoBehaviour
 {
     
-    [SerializeField] GameObject m_grid;
+    [SerializeField] Tilemap m_tileMap;
+    [SerializeField] float m_changeTime;
+
+    float m_colorAlpha = 1f;
+    Color m_color = new Color(1f,1f,1f,1f);
+    bool m_playerIn = false;
 
     void Start()
     {
@@ -16,14 +22,16 @@ public class DisapearTrigger : MonoBehaviour
 
     void Update()
     {
-        
+        ColorChange();
+        m_color = new Color(1f, 1f, 1f, m_colorAlpha);
+        m_tileMap.color = m_color;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.layer == (int)LayerName.Player)
         {
-            m_grid.SetActive(false);
+            m_playerIn = true;
         }
     }
 
@@ -31,7 +39,19 @@ public class DisapearTrigger : MonoBehaviour
     {
         if (collision.gameObject.layer == (int)LayerName.Player)
         {
-            m_grid.SetActive(true);
+            m_playerIn = false;
+        }
+    }
+
+    void ColorChange()
+    {
+        if (m_playerIn)
+        {
+            DOTween.To(() => m_colorAlpha, x => m_colorAlpha = x, 0f, m_changeTime);
+        }
+        else
+        {
+            DOTween.To(() => m_colorAlpha, x => m_colorAlpha = x, 1f, m_changeTime);
         }
     }
 }
