eb21c5a653d64e1e8508887a32e5a659ae2a8753
HideThat <101398304+HideThat@users.noreply.github.com>
2023-03-30 13:31:10 +0900

    2023-03-30

--- Assets/Requiem/Resource/Other/Item/Script/YellowKeyItem.cs
+++ Assets/Requiem/Resource/Other/Item/Script/YellowKeyItem.cs
@@ -10,7 +10,6 @@ public class YellowKeyItem : Item
         m_name = "YellowKey";
         m_ID = 2;
         gameObject.layer = (int)LayerName.Item;
-        m_image = DataController.ItemSprites[m_ID];
         m_collider = GetComponent<Collider2D>();
         m_animator = GetComponent<Animator>();
     }
